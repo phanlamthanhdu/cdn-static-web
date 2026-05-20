0b74f35115f8d12f58426ba9ae0de5808892bf94
phanlamthanhdu <phanlamthanhdu@gmail.com>
2026-05-20 22:06:24 +0700

    Update code

--- infra/aws/main.tf
+++ infra/aws/main.tf
@@ -1,7 +1,107 @@
-resource "aws_s3_bucket" "example" {
-  bucket = "duplt-testing-bucket"
+# =========================================================================
+# CONFIG CHO S3 BUCKET
+# =========================================================================
 
-  tags = {
-    project = "cdn-static-web"
+resource "aws_s3_bucket" "static_web" {
+  bucket        = var.bucket_name
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_public_access_block" "static_web_privacy" {
+  bucket = aws_s3_bucket.static_web.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_policy" "allow_cloudfront" {
+  bucket     = aws_s3_bucket.static_web.id
+  policy     = data.aws_iam_policy_document.cloudfront_s3_policy.json
+  depends_on = [aws_s3_bucket_public_access_block.static_web_privacy]
+}
+
+data "aws_iam_policy_document" "cloudfront_s3_policy" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.static_web.arn}/*"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = [aws_cloudfront_distribution.s3_distribution.arn]
+    }
+  }
+}
+
+# =========================================================================
+# CONFIG CHO CLOUDFRONT (OAC)
+# =========================================================================
+
+resource "aws_cloudfront_origin_access_control" "oac" {
+  name                              = "s3-oac-${var.bucket_name}"
+  description                       = "OAC cho phép CloudFront truy cập S3 Private"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name              = aws_s3_bucket.static_web.bucket_regional_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.oac.id
+    origin_id                = "S3-${aws_s3_bucket.static_web.bucket}"
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3-${aws_s3_bucket.static_web.bucket}"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  price_class = "PriceClass_All"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  custom_error_response {
+    error_code         = 403
+    response_code      = 200
+    response_page_path = "/index.html"
+  }
+
+  custom_error_response {
+    error_code         = 404
+    response_code      = 200
+    response_page_path = "/index.html"
   }
 }
